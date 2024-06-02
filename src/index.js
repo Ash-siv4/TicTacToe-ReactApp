844fc76588a0be3b1b340a8459a0a694cadd707f
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-02 15:50:13 +0100

    Commented the styles and index.js files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+// This file is the bridge between the App.js file and the web browser
 
-import App from "./App";
+import React, { StrictMode } from "react"; // React
+import { createRoot } from "react-dom/client"; // Library in React to talk to the web browser (React DOM)
+import "./styles.css"; // CSS file called for styling components
+
+import App from "./App"; // Call the created components created in the App.js file
 
 const root = createRoot(document.getElementById("root"));
 root.render(
