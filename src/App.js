16e62f0649a0f4b63f77df196c845b05f2c92f14
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-02 15:36:05 +0100

    Commented the App.js after implementing the Square() function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// declare a function called 'Square()'
+// export - JS keyword: allows access to the function outside of this file
+// default - JS keyword: lets other files know that this is the main function in this file
 export default function Square() {
+  // return - JS keyword: what value will be returned to the caller of the function
+  //  <button> - JSX element (JS code + HTML tags)
+  //  className="square" - button property (prop) for CSS styling
+  // X - text displayed in the button
   return <button className="square">X</button>;
 }
