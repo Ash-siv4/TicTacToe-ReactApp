e89068a267f4120eab033e5f6830128fcc7d0cc2
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-03 23:01:11 +0100

    Ineractive board to display 'X' in the clicked square

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import Square from "./UI_components/Square";
-
 // declare a function called 'Square()', later renamed to 'Board()'
 export default function Board() {
   return (
     // components can only return a single JSX element
     // to return multiple JSX elements, they need to be wrapped in Fragments: <> and </>
     // create rows with <div>'s, set className to the 'board-row' attribute defined in the styles.css file
-    // Must assign the prop a 'value' when the child component is called
+    // Must assign the prop a 'value' when the child component is called, e.g: <Square value="1" /> ---> removed when useState is applied instead
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
--- src/UI_components/Square.js
+++ src/UI_components/Square.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
+// function - JS keyword: defines a function
+// Square - function name
 // reuseable component to avoid duplication of code (moved here from App.js)
 // export - JS keyword: allows access to the function outside of this file
 // default - JS keyword: lets other files know that this is the main function in this file
-// {value} - props: property called 'value' which is passed to the child (Square) component from its parent (Board) component
-export default function Square({ value }) {
+// {value} - props: property called 'value' which is passed to the child (Square) component from its parent (Board) component ---> late replaced with USESTATE
+export default function Square() {
+  // useState - special React function for components to be able to "remember" changes
+  //   value - stores the value (starts off as "null")
+  // setValue - function used to change the value
+  const [value, setValue] = useState(null);
+  // created a function which when called, prints "clicked!" to the console (ctrl+shift+j)
+  function handleClick() {
+    // console.log("clicked!");
+    // Displays "X" on the browser when button is clicked
+    setValue("X");
+  }
   // return - JS keyword: what value will be returned to the caller of the function
-  //  <button> - JSX element (JS code + HTML tags)
-  //  className="square" - button property (prop) for CSS styling
+  // <button> - JSX element (JS code + HTML tags)
+  // className="square" - button property (prop) for CSS styling
   // X - text displayed in the button -> changed to 'value' (props) so must be in {} brackets as it needs to escape into JSX to get the value, not to print a string saying value
-  return <button className="square">{value}</button>;
+  // onClick is used to call any declared function when the button element is clicked
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
