ef24b3074c6e4e127d003bcc21fe4cbccfda5cdb
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-03 22:10:30 +0100

    Square component created

---
+++ src/UI_components/Square.js
@@ -0,0 +1,10 @@
+// reuseable component to avoid duplication of code (moved here from App.js)
+// export - JS keyword: allows access to the function outside of this file
+// default - JS keyword: lets other files know that this is the main function in this file
+export default function Square() {
+  // return - JS keyword: what value will be returned to the caller of the function
+  //  <button> - JSX element (JS code + HTML tags)
+  //  className="square" - button property (prop) for CSS styling
+  // X - text displayed in the button
+  return <button className="square">1</button>;
+}
