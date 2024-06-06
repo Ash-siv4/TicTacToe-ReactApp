6d2fe5abf2c15ffd589d57d9c7967ccbad290e38
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-06 21:52:54 +0100

    Lifted the state to the parent component and passed props to the child component

--- src/UI_components/Board.js
+++ src/UI_components/Board.js
@@ -1,26 +1,43 @@
+import { useState } from "react";
 import Square from "./Square";
 // declare a function called 'Square()', later renamed to 'Board()'
 export default function Board() {
+  // create a state variable called "squares" which is updated using "setSquares"
+  // state variable is declared as an array
+  // the array is set to a length of "9" and each element is initially set to "null"
+  //entries into the array correspond to the values in each square of the board, e.g:['O',null,'X','X','X','O','O',null,null]
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // define handleClick function in the Board instead - updates the 'squares' array regarding the board's state
+  //JS closure example: inner function (handleClick) can access variables & functions defined in a outer function (like setSquares/squares)
+  function handleclick(i) {
+    const nextSquares = squares.slice(); // creates a copy of the 'squares' array and calls it 'nextSquares'
+    nextSquares[i] = "X"; // updates the copied array and assigns 'X' to the first index - hardcoded to the upper-left square only because of the [0] so change to [i] and pass in 'i' as a function parameter
+    setSquares(nextSquares); // call 'setSquares' to say the state has changed and the BOARD and SQUARE components re-render
+  }
   return (
     // components can only return a single JSX element
     // to return multiple JSX elements, they need to be wrapped in Fragments: <> and </>
     // create rows with <div>'s, set className to the 'board-row' attribute defined in the styles.css file
     // Must assign the prop a 'value' when the child component is called, e.g: <Square value="1" /> ---> removed when useState is applied instead
+    // NOTE:
+    // onSquareClick={handleclick(0)} - infinite loop as the function is called too early and keeps rerendering the board
+    // vs.
+    // onSquareClick={() => handleclick(0)} - waits for the user to click the square and then calls the function so no infinite loop
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleclick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleclick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleclick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleclick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleclick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleclick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleclick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleclick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleclick(8)} />
       </div>
     </>
   );
