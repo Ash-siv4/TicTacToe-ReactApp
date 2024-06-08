4ff74d3524cd2bcedf42096b5be47bec501f4c39
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-08 10:19:50 +0100

    Created top-level Game component & lifted state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import Board from "./UI_components/Board";
+// import Board from "./UI_components/Board";
+// GAME is now the top-level component after implementing the "history" feature
+import Game from "./UI_components/Game";
 
 export default function App() {
-  return <Board />;
+  // return <Board />;
+  return <Game />;
 }
---
+++ src/UI_components/Game.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Board from "./Board";
+// function to display list of past moves
+export default function Game() {
+  const [xNext, setXNext] = useState(true);
+  // "Array(9).fill(null)" is within [] brackets to make an array of a single item which is an array of 9 nulls - in order to produce this(history):
+  //   [
+  //     // Before first move
+  //     [null, null, null, null, null, null, null, null, null],
+  //     // After first move
+  //     [null, null, null, null, 'X', null, null, null, null],
+  //     // After second move
+  //     [null, null, null, null, 'X', null, null, null, 'O'],
+  //     // ...
+  //   ]
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  // render the squares for the current move by reading the history array of the previous move (i.e. -1 before current index)
+  const currentSquares = history[history.length - 1];
+  // function called by board component to update the game
+  // pass in nextSquares - from Board component
+  function handlePlay(nextSquares) {
+    // ... - spread operator = Arrays: Copying, combining, and spreading elements as arguments || Objects: Copying and merging properties || Functions: Spreading array elements as individual arguments
+    setHistory([...history, nextSquares]); // creates a new array containing items from history and also items from nextSquares
+    // example: if history is [[null,null,null], ["X",null,null]] and nextSquares is ["X",null,"O"], then the new [...history, nextSquares] array will be [[null,null,null], ["X",null,null], ["X",null,"O"]]
+    setXNext(!xNext);// toggle xNext like Board used to
+  }
+  // additional "div"s are to make room for the game information, e.g. history.
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
